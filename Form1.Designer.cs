c9afb8d1cb89963f9f28a7421204fefea1688d57
lina <l1nlin.n@yandex.ru>
2024-11-04 22:15:47 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace WindowsFormsApplication1_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(5, 9);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(216, 22);
+            this.numericUpDown1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(5, 50);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(513, 271);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.EditingControlShowing += new System.Windows.Forms.DataGridViewEditingControlShowingEventHandler(this.dataGridView1_EditingControlShowing);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(585, 50);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(533, 271);
+            this.dataGridView2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(98, 393);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 40);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Ввод данных";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(210, 393);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(140, 38);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Обработка";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(354, 394);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(145, 35);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Выгрузка в Excel";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(501, 395);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(130, 32);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Выход";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1400, 536);
+            this.ControlBox = false;
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApplication1_1
+{
+    public partial class Form1 : Form
+    {
+        int n;
+        double[] X;
+        double[] Y;
+        double[,] mas;
+        public Form1()
+        {
+            InitializeComponent();
+            this.dataGridView1.EditingControlShowing += new System.Windows.Forms.DataGridViewEditingControlShowingEventHandler(this.dataGridView1_EditingControlShowing);
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        void tb_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            if (!(Char.IsDigit(e.KeyChar)) && !((e.KeyChar == '-')) && !((e.KeyChar == ',')))
+            {
+                if (e.KeyChar != (char)Keys.Back)
+                {
+                    e.Handled = true;
+                }
+            }
+        }
+        private void dataGridView1_EditingControlShowing(object sender, DataGridViewEditingControlShowingEventArgs e)
+        {
+
+            TextBox tb = (TextBox)e.Control;
+            tb.KeyPress += new KeyPressEventHandler(tb_KeyPress);
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            {
+                dataGridView1.Columns.Clear();
+                dataGridView1.ReadOnly = false;
+                dataGridView1.AllowUserToAddRows = false;
+                dataGridView1.AllowUserToDeleteRows = false;
+                n = (int)numericUpDown1.Value;
+                dataGridView1.AutoResizeColumns();
+                for (int i = 0; i < n; i++)
+                {
+                    dataGridView1.Columns.Add("New", "Y" + i.ToString());
+                }
+                dataGridView1.ColumnCount = n;
+                dataGridView1.RowCount = n;
+                X = new double[n];
+                Y = new double[n];
+                mas = new double[n, n];
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            NewMethod();
+        }
+        private void NewMethod()
+        {
+            for (int i = 0; i < n; ++i)
+                for (int j = 0; j < n; ++j)
+                    mas[i, j] = Convert.ToDouble(dataGridView1.Rows[i].Cells[j].Value);
+            for (int i = 0; i < n; i++)
+            {
+                dataGridView2.Columns.Add("New", "Y" + i.ToString());
+            }
+            dataGridView2.ColumnCount = n;
+            dataGridView2.RowCount = n;
+            for (int i = 0; i < n; ++i)
+                for (int j = 0; j < n; ++j)
+                    if (mas[i, j] % 4 == 0)
+                    {
+                        dataGridView2.Rows[i].Cells[j].Value = mas[i, j] * 1;
+                    }
+                    else
+                    {
+                        dataGridView2.Rows[i].Cells[j].Value = 0;
+                    }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int rowCounter = dataGridView2.RowCount;
+            int columnCount = dataGridView2.ColumnCount;
+            string[] line = new string[columnCount];
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Excel Files (*.xls)|*.xls";
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamWriter writer = new StreamWriter(sfd.FileName))
+                {
+                    for (int i = 0; i < dataGridView2.RowCount; i++)
+                    {
+                        for (int j = 0; j < dataGridView2.ColumnCount; j++)
+                        {
+                            line[j] = Convert.ToString(dataGridView2.Rows[i].Cells[j].Value ?? "");
+                        }
+                        writer.WriteLine(string.Join("\t", line));
+                    }
+                }
+            }
+
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
